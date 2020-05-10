a74cc333db4cabf2b70e313990451a9cfca9754b
sandernykjaer <54498944+sandernykjaer@users.noreply.github.com>
2020-05-10 23:22:02 +0200

    Add files via upload

---
+++ dist/precache-manifest.900289b1af5c7ae0c8a71a51a289ea2a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c2143b63f6d29286969",
+    "url": "/css/app.2e7cc522.css"
+  },
+  {
+    "revision": "a9e1ee39ba5bed43fd9e",
+    "url": "/css/chunk-04a4bd12.ca43f78a.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "de9de15ffe224cd33aac62384587ee27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c2143b63f6d29286969",
+    "url": "/js/app.df54651d.js"
+  },
+  {
+    "revision": "a9e1ee39ba5bed43fd9e",
+    "url": "/js/chunk-04a4bd12.04c0c93c.js"
+  },
+  {
+    "revision": "f1bbbf28586b18c3ce14",
+    "url": "/js/chunk-vendors.585ea6f2.js"
+  },
+  {
+    "revision": "afa999da32efe961a6139be38e7d844f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
